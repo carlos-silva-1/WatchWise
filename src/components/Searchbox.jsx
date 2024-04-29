1440d36ed603dd67242359c3319a684e24da346a
Carlos Silva <134824320+carlos-silva-1@users.noreply.github.com>
2024-04-29 16:53:44 -0300

    Changed width of searchbox

--- src/components/Searchbox.jsx
+++ src/components/Searchbox.jsx
@@ -2,8 +2,8 @@ import React from 'react'
 
 const Searchbox = ({ searchValue, setSearchValue }) => {
     return(
-        <div className='col-sm-3'>
-            <input className='form-control mt-4 bg-light-custom text-light searchbox' placeholder='Search'
+        <div>
+            <input className='form-control mt-3 bg-light-custom text-light searchbox' placeholder='Search'
                    value={searchValue} 
                    onChange={(event) => setSearchValue(event.target.value)}></input>
         </div>
