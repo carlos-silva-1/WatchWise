2f7c462e5616d38414a69ac79f9154d10d44a56f
Carlos Silva <134824320+carlos-silva-1@users.noreply.github.com>
2024-04-29 16:51:02 -0300

    Enlarged margins

--- src/components/IMDB.jsx
+++ src/components/IMDB.jsx
@@ -33,7 +33,7 @@ const goToIMDBPage = async (movie) => {
 const IMDB = ({ movie }) => {
     return(
         <>
-            <Button variant="outline-warning" className="p-2 ml-3 mt-1" id='imdb-btn' onClick={() => goToIMDBPage(movie)}>
+            <Button variant="outline-warning" className="p-2 ml-5 mt-2" id='imdb-btn' onClick={() => goToIMDBPage(movie)}>
                 IMDB
                 <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-up-right ml-2 mb-1" viewBox="0 0 16 16">
                     <path fill-rule="evenodd" d="M8.636 3.5a.5.5 0 0 0-.5-.5H1.5A1.5 1.5 0 0 0 0 4.5v10A1.5 1.5 0 0 0 1.5 16h10a1.5 1.5 0 0 0 1.5-1.5V7.864a.5.5 0 0 0-1 0V14.5a.5.5 0 0 1-.5.5h-10a.5.5 0 0 1-.5-.5v-10a.5.5 0 0 1 .5-.5h6.636a.5.5 0 0 0 .5-.5"/>
